2e177e8aee6a24090196d233b9cc36123e22a997
Tresor Ngahame <treezyvarrick@gmail.com>
2024-09-28 01:18:04 +0100

    modified meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Nav from "./ui/navbar";
-import Footer from "./ui/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
